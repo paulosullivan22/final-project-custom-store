6fdd54c7cd4541e72a8a2b977ba153cca901001d
paul <paulosullivan22@outlook.com>
2019-07-30 16:59:11 +0200

    backend changes for deployment

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // ADD SESSION SETTINGS HERE:
@@ -89,5 +89,10 @@ app.use('/api/auth/', auth)
 
 const fileUpload = require('./routes/fileUpload')
 app.use('/api', fileUpload)
+
+app.use((req, res) => {
+  // If no routes match, send the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 module.exports = app;
